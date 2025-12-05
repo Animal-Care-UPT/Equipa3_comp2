1d9413fa518f6f4b6777c4aba943c03f9a52b733
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2025-12-05 12:19:18 +0000

    navbar

---
+++ src/main/java/AnimalCareCentre/client/views/NavBar.java
@@ -0,0 +1,74 @@
+package AnimalCareCentre.client.views;
+
+import AnimalCareCentre.client.Navigator;
+import AnimalCareCentre.client.components.*;
+
+/**
+ * This class defines the navigation bar used throughout the platform
+ *
+ */
+public class NavBar {
+
+  private Navigator nav;
+  private ACCScene scene;
+
+  public NavBar(String loggedRole, Navigator nav, ACCScene scene) {
+    this.nav = nav;
+
+    if (loggedRole.equals("ROLE_USER")) {
+      userNavButtons();
+    } else if (loggedRole.equals("ROLE_SHELTER")) {
+      shelterNavButtons();
+    } else if (loggedRole.equals("ROLE_ADMIN")) {
+      adminNavButtons();
+    }
+  }
+
+  private void userNavButtons() {
+    ACCNavButton home = new ACCNavButton("Home");
+    ACCNavButton animals = new ACCNavButton("Search Animals");
+    ACCNavButton shelters = new ACCNavButton("Search Shelters");
+    ACCNavButton lostFound = new ACCNavButton("Lost and Found");
+    ACCNavButton acc = new ACCNavButton("Account");
+
+    home.setOnAction(e -> nav.userHomepage());
+    animals.setOnAction(e -> nav.userHomepage());
+    shelters.setOnAction(e -> nav.userHomepage());
+    lostFound.setOnAction(e -> nav.userHomepage());
+    acc.setOnAction(e -> nav.userHomepage());
+
+    scene.setHeader(home, animals, shelters, lostFound, acc);
+  }
+
+  private void adminNavButtons() {
+    ACCNavButton home = new ACCNavButton("Home");
+    ACCNavButton animals = new ACCNavButton("Search Animals");
+    ACCNavButton shelters = new ACCNavButton("Search Shelters");
+    ACCNavButton lostFound = new ACCNavButton("Lost and Found");
+    ACCNavButton acc = new ACCNavButton("Account");
+
+    home.setOnAction(e -> nav.adminHomepage());
+    animals.setOnAction(e -> nav.adminHomepage());
+    shelters.setOnAction(e -> nav.adminHomepage());
+    lostFound.setOnAction(e -> nav.adminHomepage());
+    acc.setOnAction(e -> nav.adminHomepage());
+
+    scene.setHeader(home, animals, shelters, lostFound, acc);
+  }
+
+    private void shelterNavButtons() {
+    ACCNavButton home = new ACCNavButton("Home");
+    ACCNavButton animals = new ACCNavButton("My Animals");
+    ACCNavButton lostFound = new ACCNavButton("Lost and Found");
+    ACCNavButton acc = new ACCNavButton("Account");
+
+    home.setOnAction(e -> nav.shelterHomepage());
+    animals.setOnAction(e -> nav.shelterHomepage());
+    lostFound.setOnAction(e -> nav.shelterHomepage());
+    acc.setOnAction(e -> nav.shelterHomepage());
+
+    scene.setHeader(home, animals, lostFound, acc);
+  }
+
+
+}
